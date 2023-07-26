fab19ce6490f313fd8fb417716b97e3cc0a1a4ef
Rovic Xavier Aliman <113037032+RXAliman@users.noreply.github.com>
2023-07-26 14:39:26 +0800

    Added buttons and button behavior (Non-functional)

--- scripts/fractal-maker.js
+++ scripts/fractal-maker.js
@@ -1,1 +1,31 @@
+var refPoints = 3;
+var canvas = ctx = false;
+var height = 500;
+var width = 800;
 
+function increasePoints() { 
+	refPoints++;
+	document.getElementById("f-points-text").innerHTML = refPoints;
+}
+
+function decreasePoints() {
+	if (refPoints <= 3) return;
+	
+	refPoints--;
+	document.getElementById("f-points-text").innerHTML = refPoints;
+}
+
+function setup() {
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	
+	ctx.beginPath();
+	ctx.arc(200,200,5,0,Math.PI*2);
+	ctx.fillStyle = 'red';
+	ctx.fill();
+	ctx.closePath();
+	
+	ctx.save();
+}
+
+setup();
